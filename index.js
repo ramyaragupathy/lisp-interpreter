1aa7574b61082654a30d50ccf49a26b95faf085b
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-02-22 12:47:37 +0530

    othe relational oeprators

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const repl = require('repl')
 const interpreter = require('./src/interpreter.js')
 const myEval = (cmd, context, filename, callback) => {
-  callback(null,interpreter(cmd) )
+  callback(null, interpreter(cmd))
 }
 const replServer = repl.start({
-  prompt: "lisp-interpreter > ",
+  prompt: 'lisp-interpreter > ',
   eval: myEval
 })
-
--- src/interpreter.js
+++ src/interpreter.js
@@ -1,8 +1,16 @@
 const interpret = (inp) => {
-  return numParser(inp) || strParser(inp) ||
-  mathExpParser(inp) || symbolParser(inp) || whiteSpaceParser(inp)
+  let value = numParser(inp) || strParser(inp) || boolParser(inp) ||
+  mathExpParser(inp) || whiteSpaceParser(inp)
+  if (value) {
+    if (value[1] === '\n' || value[1] === '') {
+      return value[0]
+    } else return value
+  } else return null
 }
-
+const boolParser = (input) => {
+  return input.startsWith('#t') ? ['#t', input.slice(2, input.length)]
+    : (input.startsWith('#f')) ? ['#f', input.slice(5, input.length)] : null
+}
 const digitParser = (input) => { return (input >= '0' && input <= '9') ? input : null }
 const expParser = (input) => { return (input === 'E' || input === 'e') ? input : null }
 const signParser = (input) => { return (input === '+' || input === '-') ? input : null }
@@ -66,57 +74,82 @@ const strParser = (input) => {
 const hexChars = (input) => { return ((input >= 'A' && input <= 'F') || (input >= 'a' && input <= 'f') || digitParser(input)) ? input : null }
 const specialChars = (input) => { return (input === '"' || input === '\\' || input === '/' || input === 'b' || input === 'f' || input === 'n' || input === 'r' || input === 't' || input === 'u') ? input : null }
 const whiteSpaceParser = (input) => { return (input[0] === ' ') ? [input[0], input.slice(1)] : null }
-const symbolParser = (input) => {
-  return (input[0] === '+' || input[0] === '*' || input[0] === '-' || input[0] === '/' || input[0] === '>' ||
-input[0] === '<') ? [input[0], input.slice(1)] : null
-}
-const env = {
-  '>=': (operands) => operands.reduce((a, b) => { return a >= b ? '#t' : '#f' }),
-  '<=': (operands) => operands.reduce((a, b) => { return a <= b ? '#t' : '#f' }),
-  '+': (operands) => operands.reduce((a, b) => a + b),
-  '*': (operands) => operands.reduce((a, b) => a * b),
-  '-': (operands) => operands.reduce((a, b) => a - b),
-  '/': (operands) => operands.reduce((a, b) => a / b),
-  '>': (operands) => operands.reduce((a, b) => { return a > b ? '#t' : '#f' }),
-  '<': (operands) => operands.reduce((a, b) => { return a < b ? '#t' : '#f' })
 
+const env = {
+  // '>=': (operands) => operands.reduce((a, b) => { return a === '#f' ? '#f' : (a >= b ? b : '#f') }),
+  '>=': (operands) => operands.reduce((prev, cur) => {
+    if (prev[1]) {
+      if (prev[0] >= cur[0]) {
+        return (cur === operands[operands.length - 1]) ? true : [cur[0], true]
+      } else { return (cur === operands[operands.length - 1]) ? false : [cur[0], false] }
+    } else return false
+  }),
+  '<=': (operands) => operands.reduce((prev, cur) => {
+    if (prev[1]) {
+      if (prev[0] > cur[0]) {
+        return (cur === operands[operands.length - 1]) ? true : [cur[0], true]
+      } else { return (cur === operands[operands.length - 1]) ? false : [cur[0], false] }
+    } else return false
+  }),
+  '+': (operands) => operands.reduce((prev, cur) => {
+    return typeof (prev[0]) !== 'number' || typeof (cur[0]) !== 'number' ? false : [prev[0] + cur[0]]
+  }),
+  '*': (operands) => operands.reduce((prev, cur) => {
+    return typeof (prev[0]) !== 'number' || typeof (cur[0]) !== 'number' ? false : [prev[0] * cur[0]]
+  }),
+  '-': (operands) => operands.reduce((prev, cur) => {
+    return typeof (prev[0]) !== 'number' || typeof (cur[0]) !== 'number' ? false : [prev[0] - cur[0]]
+  }),
+  '/': (operands) => operands.reduce((prev, cur) => {
+    return typeof (prev[0]) !== 'number' || typeof (cur[0]) !== 'number' ? false : [prev[0] / cur[0]]
+  }),
+  '>': (operands) => operands.reduce((prev, cur) => {
+    if (prev[1]) {
+      if (prev[0] > cur[0]) {
+        return (cur === operands[operands.length - 1]) ? true : [cur[0], true]
+      } else { return (cur === operands[operands.length - 1]) ? false : [cur[0], false] }
+    } else return false
+  }),
+  '<': (operands) => operands.reduce((prev, cur) => {
+    if (prev[1]) {
+      if (prev[0] < cur[0]) {
+        return (cur === operands[operands.length - 1]) ? true : [cur[0], true]
+      } else { return (cur === operands[operands.length - 1]) ? false : [cur[0], false] }
+    } else return false
+  }),
+  'abs': (operands) => Math.abs(...operands),
+  'mod': (operands) => operands.reduce((a, b) => a % b),
+  'operators': []
 }
 
 const mathExpParser = (input) => {
-  console.log('expression')
+  // console.log('expression')
   let operator, result
   let operands = []
+  input = checkIntermittentSpace(input)
   if (input[0] === '(') {
-    console.log('EXPRESSION START')
+    // console.log('EXPRESSION START')
     input = input.slice(1)
     if (input[0] === ')') { return '()' }
     for (var keyword in env) {
       if (input.startsWith(keyword)) {
         operator = keyword
+        env['operators'].push(operator)
         input = input.slice(keyword.length)
         break
       }
     }
-    console.log('operator ', operator)
     if (operator) {
-      console.log('INPUT ', input)
       while (input[0] !== ')') {
-        console.log('Inspecting ', input)
         result = interpret(checkIntermittentSpace(input))
-        console.log('RESULT', result)
         if (result) {
-          operands.push(result[0])
+          operands.push([result[0], true])
           input = result[1]
-          // result = (interpret(checkIntermittentSpace(result[1])))
-          // result ? operands.push(result[0]):
-          //       (operator === '+' || operator === '-')?swap(operands, 0):swap(operands, 1)
         } else return null
       }
-      console.log('LAST READ ', input[0])
       if (operator && operands && input[0] === ')') {
-        console.log('EXPRESSION END')
         let value = evaluate(operator, operands)
-        console.log('VALUE ', value)
+        // console.log('VALUE ', value)
         return [value, input.slice(1)]
       }
     } else return null
